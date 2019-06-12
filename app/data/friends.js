dc1f567aeb96b8a603ef8c42d3a011d1aee26ae2
Kyle Knox <kyleknox86@gmail.com>
2019-06-12 14:07:21 -0600

    friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsList = [{
+        name: "Biscuit",
+        photo: "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/60909419_690635251356250_1724987680732741632_n.jpg?_nc_cat=100&_nc_oc=AQn63xlhC5lqXcHPNVCE9hYzm4Yt0URbkb74a55XOooq6ufglOdYtvRTzPQElPGucSw&_nc_ht=scontent.fapa1-2.fna&oh=b8059719470d7a3261d0ddbf27cb4eaf&oe=5D7F0FEF",
+        preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+    },
+
+    {
+        name: "Gohan",
+        photo: "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/51549853_10106115925054345_2642392914201673728_n.jpg?_nc_cat=105&_nc_oc=AQnzkdmDzE0Hculjz9-PJUDqZyrA8GmSysyBSsZbbmzT-S947EWwsVkaL1tzmA1Iz9w&_nc_ht=scontent.fapa1-2.fna&oh=be2a2c72bfc25ee92688aa6497233505&oe=5D8CD596",
+        preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+    },
+
+    {
+        name: "Keanu Reeves",
+        photo: "https://i1.wp.com/metro.co.uk/wp-content/uploads/2019/04/GettyImages-801064020_1560325659.jpg?quality=90&strip=all&w=1200&h=630&crop=1&zoom=1&ssl=1",
+        preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    },
+];
+
+module.exports = friendsList;
